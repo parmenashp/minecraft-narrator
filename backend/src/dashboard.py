148c1c308986184d54b7438b8d3afd88ddb2fa1b
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2024-02-08 19:14:58 -0300

    add custom TTS dashboard tab

--- backend/src/dashboard.py
+++ backend/src/dashboard.py
@@ -1,15 +1,18 @@
+import asyncio
 import gradio as gr
 import httpx
 from io import StringIO
 from loguru import logger
 from src.config import global_config
 from src.prompts import prompt_manager
+from src.tts import tts
+from src.chatgpt import chat
 
 prompt_ids = list(prompt_manager.prompts)
 dashboard_sink = StringIO()
 
 
-def start_dashboard():
+def start_dashboard(loop: asyncio.AbstractEventLoop):
     def change_prompt(prompt_id: str, clear_context: bool):
         logger.info(f"Setting prompt to {prompt_id}")
         prompt_manager.set_current_prompt(prompt_id, clear_context)
@@ -48,6 +51,33 @@ def start_dashboard():
                 with gr.Tab(id):
                     gr.Markdown(prompt)
 
+        with gr.Tab("Custom TTS"):
+
+            def run_gpt(text):
+                logger.info(f"Custom TTS: {text}")
+                gpt = chat.ask(text)
+                return "".join(list(gpt))
+
+            def run_tts(text: str):
+                def gen():
+                    yield text
+
+                tts.synthesize(gen(), loop)
+                return "TTS audio added to queue"
+
+            gr.Interface(
+                fn=run_gpt,
+                inputs=[gr.Textbox(label="Ask gpt something")],
+                outputs="text",
+                allow_flagging="never",
+            )
+            gr.Interface(
+                fn=run_tts,
+                inputs=[gr.Textbox(label="Add tts text to queue")],
+                outputs="text",
+                allow_flagging="never",
+            )
+
         with gr.Tab("Logs"):
             gr.Code(
                 value=lambda: dashboard_sink.getvalue(),  # type: ignore
@@ -59,6 +89,4 @@ def start_dashboard():
 
     blocks.queue().launch(prevent_thread_lock=True, share=True, quiet=True)
     if global_config.discord_webhook_key:
-        httpx.post(
-            global_config.discord_webhook_key, json={"content": f"{blocks.share_url}"}
-        )
+        httpx.post(global_config.discord_webhook_key, json={"content": f"{blocks.share_url}"})
