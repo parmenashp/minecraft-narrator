7c95ff619f0bc9547ed2ef90e47271aab55321d6
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2024-02-15 05:14:32 -0300

    add speech recognition using web speech api

--- backend/src/dashboard.py
+++ backend/src/dashboard.py
@@ -1,7 +1,8 @@
 import asyncio
 import gradio as gr
 import httpx
-
+from fastapi.staticfiles import StaticFiles
+from loguru import logger
 from src.config import global_config
 from src.components.tabs.elevenlabs import elevenlabs_tab
 from src.components.tabs.customtts import customTTS_tab
@@ -19,5 +20,19 @@ def start_dashboard(loop: asyncio.AbstractEventLoop):
         elevenlabs_tab()
 
     blocks.queue().launch(prevent_thread_lock=True, share=True, quiet=True)
+
+    blocks.app.mount("/speech", StaticFiles(directory="src/speech"), name="speech")
+
     if global_config.discord_webhook_key:
         httpx.post(global_config.discord_webhook_key, json={"content": f"{blocks.share_url}"})
+
+    async def handle_websocket_microphone(websocket):
+        await websocket.accept()
+        while True:
+            data = await websocket.receive_json()
+            if data == "close":
+                break
+            if data["final"]:
+                logger.info(data)
+
+    blocks.app.add_websocket_route("/micws", handle_websocket_microphone)
